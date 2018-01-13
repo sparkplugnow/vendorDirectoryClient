571bb329b0a9d2003bf879adc722ebf85af59dc6
Israel Adura <israeladura@gmail.com>
2018-01-13 16:24:17 +0100

    integrated bulma,font-awesome and sample login form

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import '../node_modules/bulma/css/bulma.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
 import configureStore from './store/configureStore';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';

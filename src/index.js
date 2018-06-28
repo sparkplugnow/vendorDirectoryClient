945740a65497fd8b8ff0014bdea6409d6f0648c7
BigUncleYemi <olayemioyebanji911@gmail.com>
2018-06-28 11:42:12 +0100

    done ui: sign in and login page done as well as the background

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Page from './components/User';
+// import Page from './components/User';
 
-ReactDOM.render(<Page />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // change the App to Page and Uncomment the Page import to view login and sign in 
